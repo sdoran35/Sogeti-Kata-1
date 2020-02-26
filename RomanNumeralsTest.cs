754920c1fca864b6a7a95349000dba7d6db868f4
Sean <sdoran35@gmail.com>
2020-02-25 23:47:25 -0600

    Finished Roman Numeral Kata.

--- RomanNumeralsTest.cs
+++ RomanNumeralsTest.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace RomanNumeralsCodingKata
 {
@@ -9,10 +10,83 @@ namespace RomanNumeralsCodingKata
         [Fact]
         public void GivenArabicNumber1ReturnRomanNumeralI()
         {
-            var items = 1;
-            var expected = "I";
+            1.ArabicToRoman("I");
+        }
+
+        [Fact]
+        public void GivenArabicNumber3ReturnRomanNumeralIII()
+        {
+            3.ArabicToRoman("III");
+        }
+
+        
+
+
+        [Fact]
+        public void GivenArabicNumber9ReturnRomanNumeralIX()
+        {
+            9.ArabicToRoman("IX");
+        }
+
+        [Fact]
+        public void GivenArabicNumer1066ReturnRomanNumeralMLXVI()
+        {
+           1066.ArabicToRoman("MLXVI");
             
-            Assert.Equal(expected,new RomanNumerals().ConvertArabicToRoman(items);
+        }
+
+        [Fact]
+        public void GivenArabicNumber1989ReturnRomanNumeralMCMLXXXIX()
+        {
+            1989.ArabicToRoman("MCMLXXXIX");
+        }
+
+
+        [Fact]
+        public void GivenRomanIReturn1()
+        {
+            "I".RomanToArabic(1);
+        }
+
+        [Fact]
+        public void GivenRomanIIIReturn3()
+        {
+            "III".RomanToArabic(3);
+        }
+
+        [Fact]
+        public void GivenRomanIXReturn9()
+        {
+            "IX".RomanToArabic(9);
+        }
+
+        [Fact]
+        public void GivenRomanMLXVIReturn1066()
+        {
+            "MLXVI".RomanToArabic(1066);
+        }
+
+        [Fact]
+        public void GivenRomanMCMLXXXIXReturn1989()
+        {
+            "MCMLXXXIX".RomanToArabic(1989);
+        }
+
+    }
+    
+    internal static class TestHelper
+    {
+        public static void ArabicToRoman(this int input, string expected)
+        {
+            var item = new RomanNumerals();
+            Assert.Equal(expected,RomanNumerals.ArabicToRoman(input));
+            
+        }
+
+        public static void RomanToArabic(this string input, int expected)
+        {
+            var item = new RomanNumerals();
+            Assert.Equal(expected,RomanNumerals.RomanToArabic(input));
         }
     }
 }
